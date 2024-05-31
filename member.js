0ad15ebb46e556e49e6af7db63d16a90c6211a71
tayabr <97274268+tayabr@users.noreply.github.com>
2024-05-31 23:03:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    let skills = [
+        "C++",
+        "C#",
+        "Java",
+        "Python",
+        "JavaScript",
+        "HTML",
+        "CSS"
+    ]
+
+    return skills;
+}
